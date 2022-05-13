7f28f6e9bc52adde38fddaf86ef796a8c8644cee
Phat <phatdroid94@gmail.com>
2022-05-13 18:20:16 +0700

    Initial commit

---
+++ app/src/main/java/com/example/retrofit18022022/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.retrofit18022022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Protocol;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                            .readTimeout(30, TimeUnit.SECONDS)
+                            .writeTimeout(30 , TimeUnit.SECONDS)
+                            .connectTimeout(30 ,TimeUnit.SECONDS)
+                            .protocols(Arrays.asList(Protocol.HTTP_1_1))
+                            .build();
+        Gson gson = new GsonBuilder().setLenient().create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://khoapham.vn/")
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        ApiService apiService = retrofit.create(ApiService.class);
+
+        Call<Demo1> fetchDemo1 = apiService.fetchDemo1();
+        fetchDemo1.enqueue(new Callback<Demo1>() {
+            @Override
+            public void onResponse(Call<Demo1> call, Response<Demo1> response) {
+                Demo1 demo1 = response.body();
+                Log.d("BBB",demo1.toString());
+            }
+
+            @Override
+            public void onFailure(Call<Demo1> call, Throwable t) {
+
+            }
+        });
+    }
+}
